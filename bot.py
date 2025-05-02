a78f14d16585bc3f34772d6367280cf08d2a9704
Saleh <90215881+SalehH7@users.noreply.github.com>
2025-05-03 00:39:33 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -39,8 +39,6 @@ logging.info("🚀 Bot is starting...")
 
 os.environ["EASYOCR_CACHE_DIR"] = "/tmp/easyocr_models"
 reader = easyocr.Reader(['ar', 'en'], download_enabled=True)
-print("✅ Bot token loaded:", TELEGRAM_TOKEN[:10], "...")
-logging.info("🚀 Bot is starting...")
 import json
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 service_account_info = json.loads(os.getenv("service_account"))
